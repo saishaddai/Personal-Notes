bdc38c70c96f15982b37f194f5b1c16b7e2adf07
Saidel Lopez <sailopher@yahoo.com.mx>
2019-04-08 14:03:47 -0500

    Update react_native_instructions.sh

--- scripts/react_native_instructions.sh
+++ scripts/react_native_instructions.sh
@@ -23,7 +23,39 @@ npm install -g create-react-native-app
 cd directory-to-workspace/
 
 ##create a react native app with name MyApp
+##The command above might fail since it requires Expo CLI installed the first time
+##Even so, the command will ask you to install it  
+##Also will ask you to name your app in a JSON file, this name will be seeing as the app's name
+##Also will ask you to install yarn dependencies if the process finds you have yarn installed
 create-react-native-app MyApp
+
 
 ##cd MyApp
 ##you can start to work here. A new structure was created for you with basic functionality
+
+
+## to start your service. Pick one
+##yarn start
+##nom start
+
+##to install yarn
+brew install yarn
+
+
+##Best practive tip. Once in the app directory it is a good practice to create teh following directories
+##To keep all the code separated anbd well organised. In a new tab go to the app's directory
+mkdir components
+mkdir config
+mkdir navigation
+mkdir redux
+mkdir screens
+
+
+##Also, remember that every file must have an export for each module or class, 
+##Only on e export dafault is allowed and all the imports MUST finish with semicolon
+##The styles variables should be exported as module.exports = styles (where styles is the name of the styles var)
+##Remember to always save all the files since it won't be loaded properly in the emulator until you save it
+
+##install react-navigation
+npm install react-navigation --save
+
